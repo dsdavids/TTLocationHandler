1b5ad90a0066d1b6e9504a2dfd8714733ee39094
Dean S. Davids <dnod@aol.com>
2012-10-13 13:46:31 -0400

    Added license info to all class files

--- LocationHandler/TTLocationHandler.h
+++ LocationHandler/TTLocationHandler.h
@@ -1,8 +1,27 @@
 // TTLocationHandler.h
 //
 //  Created by Dean Davids on 3/29/12.
-//  Copyright (c) 2012 Dean S. Davids, dba Tailgate Technologies. All rights reserved.
+//  Copyright (c) 2012 Dean S. Davids, Tailgate Technology. All rights reserved.
 //
+// Portions of this software Copyright (c) 2010, Long Weekend LLC
+// Long Weekend LLC credit for basis of the origination of this code.
+// Original code and tutorial for same can be found at http://longweekendmobile.com/2010/06/30/location-region-data-in-background-on-ios4-iphone/
+//
+// All of the code in this file shares same permissions as follows:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial
+// portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
